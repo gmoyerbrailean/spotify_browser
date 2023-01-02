1dda9429e6d1d8b48efc0c7349b467ad6c8bbc61
Gregory Moyerbrailean <gregory.moyerbrailean@gmail.com>
2023-01-01 20:05:09 -0800

    fixed chart options for date histogram component

--- frontend/src/compoments/charts/DateHistogram.js
+++ frontend/src/compoments/charts/DateHistogram.js
@@ -19,72 +19,51 @@ class DateHistogram extends React.Component {
         // Map the timestamps into the date label format
         // and count the number of plays per date
         const date_frequency = date_labels.map(date => {
-            return timestamps.reduce((n, val) => {
-              let dt = new Date(val);
-              return n + (months[dt.getMonth()] + " " + dt.getFullYear() === date);
-            }, 0);
+          return timestamps.reduce((n, val) => {
+            let dt = new Date(val);
+            return n + (months[dt.getMonth()] + " " + dt.getFullYear() === date);
+          }, 0);
         });
         
         const data = {
-            labels: date_labels,
-            datasets: [{
-              data: date_frequency,
-              backgroundColor: 'rgba(29, 185, 84, 0.5)' // spotify green
-            }]
-          }
+          labels: date_labels,
+          datasets: [{
+            data: date_frequency,
+            backgroundColor: 'rgba(29, 185, 84, 0.5)' // spotify green
+          }]
+        }
 
-        
-        // const data = {
-        //   labels: ["1", "2", "3", "4", "5"],
-        //   datasets: [{
-        //     data: [2,3,2,2,4],
-        //     backgroundColor: 'rgba(29, 185, 84, 0.5)' // spotify green
-        //   }]
-        // }
-
-          const chartTitle = "";
-          const options = {
-              responsive: true,
-              maintainAspectRatio: false,
+        const chartTitle = this.props.chartTitle;
+        const options = {
+            title: {
+              display: chartTitle ? true : false,
+              text: chartTitle
+            },
+            scales: {
+              x: {
+                type: 'category',
+                labels: date_labels
+              },
+              y: {
+                type: 'linear',
+                beginAtZero: true,
+                grace: "5%"
+              }
+            },
+            plugins: {
               legend: {
                 display: false
-              },
-              title: {
-                display: chartTitle ? true : false,
-                text: chartTitle
-              },
-              scales: {
-                yAxes: [{
-                    ticks: {
-                      beginAtZero: true,
-                    },
-                    scaleLabel: {
-                      display: true,
-                      labelString: '# Plays'
-                    }        
-                }],
-                xAxes: [{
-                  ticks: {
-                    beginAtZero: true,
-                  }
-                }]
               }
             }
-
-            console.log(data);
-      
-          return(
-            <Bar
-              data={data}
-              // options={options}
-              // height={chart_height}
-            />      
-          )
-          // return(<p>hello</p>);
-
+          }
+    
+        return(
+          <Bar
+            data={data}
+            options={options}
+          />      
+        )
     }
-
-
 };
 
 export default DateHistogram;
